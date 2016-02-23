5d8e3e4c471fc2303ba3413a4ecfe47d8f978b38
Raf <rafhaan@gmail.com>
2016-02-23 20:58:42 +0000

    Cleaned up the Initial GUI Class, separated into multiple methods and got rid of unnecessary lines

--- PRACoursework/src/com/theinfiniteloop/sharktracker/gui/InitialFrame.java
+++ PRACoursework/src/com/theinfiniteloop/sharktracker/gui/InitialFrame.java
@@ -3,7 +3,6 @@ package com.theinfiniteloop.sharktracker.gui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,26 +18,46 @@ public class InitialFrame {
 	private JLabel label;
 
 	public InitialFrame() {
+		createLabels();
+		createButtons();
+		createFrame();
+	}
 
+	public void createFrame() {
 		JFrame frame = new JFrame();
 		frame.setTitle("Shark Tracker");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		BoxLayout boxLayout = new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS);
 
+		frame.add(logo);
+		frame.add(label);
+		frame.add(new Box.Filler(new Dimension(10, 20), new Dimension(10, 20), new Dimension(10, 20)));
+		frame.add(searchButton);
+		frame.add(new Box.Filler(new Dimension(10, 10), new Dimension(10, 10), new Dimension(10, 10)));
+		frame.add(favsButton);
+
+		frame.setLayout(boxLayout);
+		frame.getContentPane().setBackground(Color.white);
+		frame.setSize(new Dimension(300, 400));
+		frame.setVisible(true);
+	}
+
+	public void createLabels() {
 		BufferedImage logoPicture = null;
 		try {
 			logoPicture = ImageIO.read(new File("Shark Logo.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		logo = new JLabel(new ImageIcon(logoPicture.getScaledInstance(200, 200, logoPicture.SCALE_DEFAULT)));
 		logo.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		label = new JLabel("Shark Tracker");
 		label.setAlignmentX(Component.CENTER_ALIGNMENT);
+	}
 
+	public void createButtons() {
 		Border border = BorderFactory.createLineBorder(Color.black, 2);
 
 		searchButton = new JButton("Search");
@@ -46,28 +65,16 @@ public class InitialFrame {
 		searchButton.setMaximumSize(new Dimension(250, 50));
 		searchButton.setBorder(border);
 		searchButton.setBackground(Color.white);
-		
+
 		favsButton = new JButton("Favourites");
 		favsButton.setEnabled(false);
 		favsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		favsButton.setMaximumSize(new Dimension(250, 50));
 		favsButton.setBorder(border);
 		favsButton.setBackground(Color.white);
-
-		frame.setLayout(boxLayout);
-		frame.add(logo);
-		frame.add(label);
-		frame.add(new Box.Filler(new Dimension(10, 20), new Dimension(10, 20), new Dimension(10, 20)));
-		frame.add(searchButton);
-		frame.add(new Box.Filler(new Dimension(10, 10), new Dimension(10, 10), new Dimension(10, 10)));
-		frame.add(favsButton);
-		
-		frame.getContentPane().setBackground(Color.white);
-		frame.setSize(new Dimension(300, 400));
-		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		InitialFrame i = new InitialFrame();
+		InitialFrame frame = new InitialFrame();
 	}
 }
