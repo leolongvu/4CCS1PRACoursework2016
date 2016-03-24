b5adda353202938cc7620a2fbe6141fe6b827730
Laura-Iconia <laura-iconia.lala@kcl.ac.uk>
2016-03-24 23:10:36 +0000

    Javadoc for StatisticsFrame class added.

--- PRACoursework/src/com/theinfiniteloop/sharktracker/gui/StatisticsFrame.java
+++ PRACoursework/src/com/theinfiniteloop/sharktracker/gui/StatisticsFrame.java
@@ -14,6 +14,15 @@ import org.jfree.data.general.DefaultPieDataset;
 import com.theinfiniteloop.sharktracker.api.SharkTime;
 import com.theinfiniteloop.sharktracker.controller.Controller;
 
+/**
+ * 
+ * @author Laura
+ * 
+ *         A class which creates pie charts that indicate the number of sharks
+ *         in the tracking range selected for each possible option (Gender,
+ *         Stage of Life, Tag of Location). I had to import jcommon and
+ *         jfreechart libraries in order to be able to create the pie charts.
+ */
 public class StatisticsFrame {
 
 	private Controller controller;
@@ -41,6 +50,18 @@ public class StatisticsFrame {
 
 	private int[] numberByLocation;
 
+	/**
+	 * Constructor initializes the fields and creates the Statistics frame with
+	 * the pie charts when called.
+	 * 
+	 * @param rangeChosen
+	 *            A String input which represents the option chosen in the
+	 *            "Tracking Range"
+	 * @param controller
+	 *            The "StatisticsFrame" class uses the controller(gives a
+	 *            reference to Query) to call the methods from the "Query"
+	 *            class.
+	 */
 	public StatisticsFrame(String rangeChosen, Controller controller) {
 		statistics = new JFrame();
 		this.controller = controller;
@@ -54,7 +75,6 @@ public class StatisticsFrame {
 		getSharkList = new ArrayList<SharkTime>();
 		getSharkList = controller.getSharkByTimeFrame(rangeChosen);
 		totalCount = getSharkList.size();
-		System.out.println(totalCount);
 		messageGender = new JLabel();
 		messageLife = new JLabel();
 		messageLocation = new JLabel();
@@ -70,6 +90,12 @@ public class StatisticsFrame {
 		statistics.setDefaultCloseOperation(statistics.DISPOSE_ON_CLOSE);
 	}
 
+	/**
+	 * Void method creates pie chart for the gender of sharks. Calls
+	 * getNumberOfSharkByGender() method (commented below). If there are any
+	 * sharks found it creates the pie chart relevant to the data. Otherwise a
+	 * message shows up.
+	 */
 	public void createGenderChart() {
 		getNumberOfSharksByGender();
 		if (femaleCount + maleCount != 0) {
@@ -89,6 +115,12 @@ public class StatisticsFrame {
 		}
 	}
 
+	/**
+	 * Void method creates pie chart for the stage of life of the sharks. Calls
+	 * getNumberOfSharkByStageOfLife() method (commented below). If there are
+	 * any sharks found it creates the pie chart relevant to the data. Otherwise
+	 * a message shows up.
+	 */
 	public void createStageOfLifeChart() {
 		getNumberOfSharksByStageOfLife();
 		if (matureCount + immatureCount + undeterminedCount != 0) {
@@ -112,13 +144,18 @@ public class StatisticsFrame {
 		}
 	}
 
+	/**
+	 * Void method creates pie chart for the location of sharks. Calls
+	 * getNumberOfSharkByLocationr() method (commented below). If there are any
+	 * sharks found it creates the pie chart relevant to the data. Otherwise a
+	 * message shows up.
+	 */
 	public void createLocationChart() {
 		int[] numberOfSharks = getNumberOfSharksByLocation();
 		int totalNo = 0;
 		for (int s : numberOfSharks) {
 			totalNo = s + totalNo;
 		}
-		System.out.println(numberOfSharks.length);
 		if (totalNo != 0) {
 			data = new DefaultPieDataset();
 			int i = 0;
@@ -137,34 +174,47 @@ public class StatisticsFrame {
 		}
 	}
 
-	public DefaultPieDataset getData() {
-		return data;
-	}
-
+	/**
+	 * Void method which gets the number of male sharks and male sharks,
+	 * respectively. Uses an ArrayList of type SharkTime to store all the male
+	 * sharks. Computes the number of female sharks by subtracting the number of
+	 * male sharks from the total number of sharks.
+	 */
 	public void getNumberOfSharksByGender() {
 		ArrayList<SharkTime> getSharkListTest = controller.getSharkByGender("Male", getSharkList);
 		maleCount = getSharkListTest.size();
 		femaleCount = totalCount - maleCount;
-		System.out.println(maleCount + " " + femaleCount);
-
 	}
 
+	/**
+	 * Void method which gets the number of mature sharks, immature sharks and
+	 * sharks which have undetermined gender. Uses an ArrayList of type
+	 * SharkTime to store all the mature sharks and immature sharks,
+	 * respectively. Computes the number of undetermined sharks by subtracting
+	 * the number of mature and immature sharks from the total number of sharks.
+	 */
 	public void getNumberOfSharksByStageOfLife() {
 		ArrayList<SharkTime> getSharkListTest = controller.getSharkByLifeStage("Mature", getSharkList);
 		matureCount = getSharkListTest.size();
 		getSharkListTest = controller.getSharkByLifeStage("Immature", getSharkList);
 		immatureCount = getSharkListTest.size();
 		undeterminedCount = totalCount - matureCount - immatureCount;
-		System.out.println(matureCount + " " + immatureCount + " " + undeterminedCount);
 	}
 
+	/**
+	 * Method which returns the number of sharks in a certain location. Uses a
+	 * for-each loop which iterates through all the String locations in the
+	 * "locations" ArrayList and stores in an ArrayList of type SharkTime all
+	 * the sharks in the "s" locations.
+	 * 
+	 * @return int Array which stores the number of shaks by location
+	 */
 	public int[] getNumberOfSharksByLocation() {
-		int i = 0;
+		int i = 0;// number of the location
 		numberByLocation = new int[locations.size()];
 		for (String s : locations) {
 			ArrayList<SharkTime> getSharkListTest = controller.getSharkByLocation(s, getSharkList);
 			numberByLocation[i] = getSharkListTest.size();
-			System.out.println(s + " " + numberByLocation[i]);
 			i++;
 		}
 		return numberByLocation;
